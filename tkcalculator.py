7a5c9b9f2dcd4f62225c988705c9bd24db94adfc
giri-netha <116621307+giri-netha@users.noreply.github.com>
2023-01-29 08:36:39 +0530

    Create tkcalculator.py

---
+++ tkcalculator.py
@@ -0,0 +1,40 @@
+from tkinter import *
+root=Tk()
+root.geometry("400x320")
+root.title("calculator")
+e=Entry(root,font=20,width=30,borderwidth=4)
+e.grid(row=0,column=0,columnspan=3,padx=40,pady=10)
+def click(number):
+	# e.delete(0,END)
+	e.insert(0,number)
+def click_clear():
+	e.delete(0,END)
+def click_equal():
+	return
+button=Button(root,text="9",font=20,width=5,height=2,padx=20,pady=10,command=lambda:click(9))
+button1=Button(root,text="8",font=20,width=5,height=2,padx=20,pady=10,command=lambda:click(8))
+button2=Button(root,text="7",font=20,width=5,height=2,padx=20,pady=10,command=lambda:click(7))
+button3=Button(root,text="6",font=20,width=5,height=2,padx=20,pady=10,command=lambda:click(6))
+button4=Button(root,text="5",font=20,width=5,height=2,padx=20,pady=10,command=lambda:click(5))
+button5=Button(root,text="4",font=20,width=5,height=2,padx=20,pady=10,command=lambda:click(4))
+button6=Button(root,text="3",font=20,width=5,height=2,padx=20,pady=10,command=lambda:click(3))
+button7=Button(root,text="2",font=20,width=5,height=2,padx=20,pady=10,command=lambda:click(2))
+button8=Button(root,text="1",font=20,width=5,height=2,padx=20,pady=10,command=lambda:click(1))
+button9=Button(root,text="0",font=20,width=5,height=2,padx=20,pady=10,command=lambda:click(0))
+button_clear=Button(root,text="clear",font=20,width=5,height=2,padx=20,pady=10,command=lambda:click_clear())
+button_equal=Button(root,text="=",font=20,width=5,height=2,padx=20,pady=10,command=lambda:click_equal())
+
+button.grid(row=1,column=0)
+button1.grid(row=1,column=1)
+button2.grid(row=1,column=2)
+button3.grid(row=2,column=0)
+button4.grid(row=2,column=1)
+button5.grid(row=2,column=2)
+button6.grid(row=3,column=0)
+button7.grid(row=3,column=1)
+button8.grid(row=3,column=2)
+button9.grid(row=4,column=0)
+button_clear.grid(row=4,column=1)
+button_equal.grid(row=4,column=2)
+
+root.mainloop()
